a2ffdbb690230c9fe9476749c166cbb3c485edab
Matt Lewellyn <guruofgentoo@gmail.com>
2022-02-25 14:51:05 -0500

    change plugin spec requirement from keg to flask - nothing keg-specific here for dependencies fixes #30

--- keg_storage/__init__.py
+++ keg_storage/__init__.py
@@ -1,7 +1,7 @@
 from keg_storage.version import VERSION as __version__  # noqa
 from importlib.util import find_spec as _find_spec
 
-if _find_spec('keg'):
+if _find_spec('flask'):
     from keg_storage.plugin import (  # noqa: F401
         Storage,
         LinkViewMixin,
